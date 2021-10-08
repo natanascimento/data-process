087e470a87711e9c117a0100cfe9fd5ec31c17c2
Natan Nascimento <natanascimentom@outlook.com>
2021-10-08 11:52:38 -0300

    feat: add data pipeline

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
-from database.tables.ddl import DDLFactory
+from pipe import DataPipeline
 
 
 if __name__ == '__main__':
   
-  DDLFactory(operation="delete-operational")
-  DDLFactory(operation="create-operational")
-  DDLFactory(operation="insert-operational")
-  DDLFactory(operation="delete-dw")
-  DDLFactory(operation="create-dw")
+  print(DataPipeline().execute)
