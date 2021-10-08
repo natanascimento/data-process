b6b2ec5ea224149cf930b532265376836baa80c6
Natan Nascimento <natanascimentom@outlook.com>
2021-10-08 11:33:58 -0300

    feat: move sql directories

--- app/core/config/settings.py
+++ app/core/config/settings.py
@@ -13,3 +13,11 @@ class Settings:
   ORACLE_PORT = os.getenv('ORACLE_PORT')
   ORACLE_SERVICE_NAME = os.getenv('ORACLE_SERVICE_NAME')
   
+  ROOT_DIR = dirname(dirname(dirname(abspath(__file__))))
+  DATABASE_DIR = join(ROOT_DIR ,"database")
+  SCRIPTS_DIR = join(DATABASE_DIR ,"scripts")
+  CREATE_OP_DIR = join(SCRIPTS_DIR, "02-school-operational-db.sql")
+  CREATE_DW_DIR = join(SCRIPTS_DIR, "05-school-dw-db.sql")
+  DROP_OP_DIR = join(SCRIPTS_DIR, "03-school-operation-drop.sql")
+  DROP_DW_DIR = join(SCRIPTS_DIR, "06-school-dw-drop.sql")
+  INSERT_OP_DIR = join(SCRIPTS_DIR, "04-school-dump-db.sql")
