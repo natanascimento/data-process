fec2a958849d2dec25741f938c2440d5bf4e00d7
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2021-10-07 22:34:02 -0300

    feat: added db dump for 'disciplinas' and 'matrizes curso'

--- app/database/scripts/04-school-dump-db.sql
+++ app/database/scripts/04-school-dump-db.sql
@@ -16,95 +16,51 @@ insert into cursos ( cod_curso, nom_curso, cod_dpto ) values ( 0008, 'Farmácia'
 insert into cursos ( cod_curso, nom_curso, cod_dpto ) values ( 0009, 'Direito', 002 );
 insert into cursos ( cod_curso, nom_curso, cod_dpto ) values ( 0010, 'Sistemas de Informação', 001 );
 /*=======================================*/ 
---INGEST DATA FOR "Disciplinas" (FALTA DAR OS NOMES DAS DISCIPLINAS E CARGA HORARIA DAS MESMAS) 
-
-carga_horaria = [20, 40, 80]
-
-Ciências da Computação
-
+--INGEST DATA FOR "Disciplinas" ( CARGA HORARIA DAS MESMAS) 
 insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00001, 'Redes de Computadores', 80.0 );
 insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00002, 'Banco de Dados', 40.0 );
-
-
-Matemática
-
 insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00003, 'Calculo II', 80.0 );
 insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00004, 'Calculo I', 40.0 );
-
-Enfermagem
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Design de Interiores
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Engenharia Mecanica
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Arquitetura
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Nutrição
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, ' ', 40.0 );
-
-Farmácia
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Direito
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
-Sistemas de Informação
-
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, '', 80.0 );
-insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, '', 40.0 );
-
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00005, 'Anatomia Humana', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00006, 'Farmacologia', 40.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00007, 'Ergonomia', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00008, 'Desenho Técnico', 40.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00009, 'Mecânica geral', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00010, 'Álgebra linear', 40.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00011, 'Gestão de Projetos', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00012, 'Urbanismo e Infraestrutura', 40.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00013, 'Nutrição Humana', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00014, 'Avaliação Nutricional', 20.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00015, 'Biologia Celular', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00016, 'Manipulação', 20.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00017, 'Introdução ao Direito', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00018, 'Direito Romano', 40.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00019, 'Engenharia de Software', 80.0 );
+insert into disciplinas ( cod_disc, nome_disc, carga_horaria ) values ( 00020, 'Laboratório de banco de dados', 40.0 );
 /*=======================================*/  
--- INGEST DATA FOR "Matrizes Cursos" (FALTA ORGANIZAR DUAS MATERIAS POR CURSO)
+-- INGEST DATA FOR "Matrizes Cursos" 
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0001, 00004, 1 );
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0001, 00002, 1 );
-
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0002, 00004, 1 );
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0002, 00003, 2 );
-
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0003, 00005, 1 );
 insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0003, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0004, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0004, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0005, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0005, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0006, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0006, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0007, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0007, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0008, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0008, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0009, 00005, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0009, 00006, 1 );
-
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0010, 00001, 1 );
-insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0010, 00004, 1 );
-
-/**********************************************************************************/
--- Inserção na Tabela de Alunos
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0004, 00007, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0004, 00008, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0005, 00009, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0005, 00010, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0006, 00011, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0006, 00012, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0007, 00013, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0007, 00014, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0008, 00015, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0008, 00016, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0009, 00017, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0009, 00018, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0010, 00019, 1 );
+insert into matrizes_cursos ( cod_curso, cod_disc, periodo ) values ( 0010, 00020, 1 );
+/*=======================================*/  
+-- INGEST DATA FOR "Alunos" 
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001230, 'Taíssa', TO_DATE('2019-01-01', 'yyyy/mm/dd'), 0001, 'N' );
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001231, 'Josiane', TO_DATE('2020-12-02', 'yyyy/mm/dd'), 0001, 'S' );
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001232, 'Elielson', TO_DATE('2019-12-03', 'yyyy/mm/dd'), 0001, 'N' );
@@ -134,4 +90,4 @@ insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 11
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001256, 'Vivian', TO_DATE('2020-06-10', 'yyyy/mm/dd'), 0009, 'S' );
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001257, 'Luisa', TO_DATE('2021-06-08', 'yyyy/mm/dd'), 0010, 'N' );
 insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001258, 'Prince', TO_DATE('2021-06-09', 'yyyy/mm/dd'), 0010, 'N' );
-insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 118700129, 'Marlon', TO_DATE('2021-06-10', 'yyyy/mm/dd'), 0010, 'N' );
+insert into alunos ( mat_alu, nome, dat_entrada, cod_curso, cotista ) values( 1187001259, 'Marlon', TO_DATE('2021-06-10', 'yyyy/mm/dd'), 0010, 'N' );
